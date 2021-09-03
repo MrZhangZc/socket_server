85208d57ab2a54eb6f63d2407877d5d700ed396c
zzc <1761997216@qq.com>
2021-09-03 17:00:03 +0800

    fix:chat

--- app/socket/index.js
+++ app/socket/index.js
@@ -6,63 +6,47 @@ let count = 0;
 // 总访客人数
 let totalCount = 0;
 
-let onLine = []
-
 const socketServer = server => {
   const io = require('socket.io')(server);
   io.on('connection', (socket) => {
     console.log("当前有用户连接");
     let name = ''
-    count++;
     totalCount++;
-    console.log("count:" + count);
 
+    // 加入群聊
     socket.on("join", async function(message) {
+      const res = await redis.hexists(Chat_List, socket.id)
+      if(!res) count++;
       name = message.name;
       await redis.hset(Chat_List, socket.id, JSON.stringify({name, avatar: message.avatar}))
-      console.log(name + "加入了群聊");
-      onLine.push({
-        name: name,
-        avatar: message.avatar,
-      })
+      const list = await redis.hgetall(Chat_List)
       socket.broadcast.emit("joinNoticeOther", {
         name: name,
-        avatar: message.avatar,
         action: "加入了群聊",
-        count: count
-      });
-      const list = await redis.hgetall(Chat_List)
-      socket.emit("joinNoticeSelf", {
+        count: count,
         onLineList: list
       });
-    });
-
-    socket.on("disconnect", function() {
-      redis.hdel(Chat_List, socket.id)
-      count--
-      // onLine = onLine.filter(item => )
-      console.log("user disconnected", count);
-    });
-
-    //Someone is typing
-    socket.on("typing", data => {
-      socket.broadcast.emit("notifyTyping", {
-        user: data.user,
-        message: data.message
+      socket.emit("joinNoticeSelf", {
+        onLineList: list,
+        count: count,
       });
     });
 
-    socket.on("disconnectUser", function(message){
-      console.log(message, '离开了')
-    })
-
-    // 给客户端发送消息
-    socket.emit('welcome','欢迎连接sockeiiiiiooooo')
-      
-    // 监听客户端消息
-    socket.on('hello', data => {
-      console.log('接收客户端发送数据', data)
-    })
+    // 断开连接
+    socket.on("disconnect", async function() {
+      const res = await redis.hexists(Chat_List, socket.id)
+      if(res) {
+        await redis.hdel(Chat_List, socket.id)
+        count--
+        const list = await redis.hgetall(Chat_List)
+        socket.broadcast.emit("joinNoticeOther", {
+          name: name,
+          action: "离开了群聊",
+          count: count,
+          onLineList: list
+        });
+      }
+    });
 
     socket.on('sendGroupMsg', data => {
       console.log('收到消息', data)
@@ -72,7 +56,14 @@ const socketServer = server => {
       chatMessage.save();
     })
 
-    //when soemone stops typing
+    //Someone is typing
+    socket.on("typing", data => {
+      socket.broadcast.emit("notifyTyping", {
+        user: data.user,
+        message: data.message
+      });
+    });
+
     socket.on("stopTyping", () => {
       socket.broadcast.emit("notifyStopTyping");
     });
